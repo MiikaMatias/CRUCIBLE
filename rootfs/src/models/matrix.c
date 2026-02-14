3f2e004cb2745e9b700060ac599a15d9d68b131c
MiikaMatias <miikapiiparinen24@gmail.com>
2026-02-14 10:20:42 +0200

    matrix-lib: transpose

---
+++ rootfs/src/models/matrix.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    size_t cols;
+    size_t rows;
+    double* data;
+} matrix;
+
+matrix* create_zero_matrix(size_t n_rows, size_t n_cols) {
+    matrix *m = (matrix *)malloc(sizeof(matrix));
+    m->rows = n_rows;
+    m->cols = n_cols;
+    m->data = (double *)malloc(n_rows * n_cols * sizeof(double));
+
+    for (int i = 0; i < n_rows * n_cols; i++) {
+        m->data[i] = 0.0;
+    }
+
+    return m;
+}
+
+// https://gudok.xyz/transpose/
+// when needed, optimize with the good old fashioned
+// block -> vectorize workflow
+matrix* transpose(matrix* original_matrix) {
+    matrix *transposed_matrix = (matrix *) malloc(sizeof(matrix));
+    transposed_matrix->cols = original_matrix->rows;
+    transposed_matrix->rows = original_matrix->cols;
+    transposed_matrix->data = (double *)malloc(transposed_matrix->cols * transposed_matrix->rows * sizeof(double));
+
+    for (int r = 0; r < original_matrix->rows; r++) {
+        for (int c = 0; c < original_matrix->cols; c++) {
+            transposed_matrix->data[r * original_matrix->cols + c] = original_matrix->data[c * original_matrix->rows + r];
+        }
+    }
+
+    return transposed_matrix;
+}
+
+void print_matrix(matrix* m) {
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            printf("%lf ", m->data[i*m->cols + j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char** argv) {
+    matrix *m = create_zero_matrix(5, 4);
+    m->data[2] = 1.0;
+    matrix *t = transpose(m);
+    printf("Original %zuX%zu:\n", m->rows, m->cols);
+    print_matrix(m);
+    printf("Transpose %zuX%zu:\n", t->rows, t->cols);
+    print_matrix(t);
+    exit(0);
+}
