33773e94191d4111aee14a40b1453ae87347633d
MiikaMatias <miikapiiparinen24@gmail.com>
2026-02-18 17:59:08 +0200

    basic-utils: remove comment from mv

--- rootfs/src/basic_utils/mv.c
+++ rootfs/src/basic_utils/mv.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    /*
-    O_CREAT: create file if not exist
-    O_WRONLY: write only
-    O_TRUNC: if exists, truncate to 0 
-    S_IRUSR: read user
-    S_IWUSR: write user
-    */
     char *original_file_name = argv[1];
     char *new_file_name = argv[2];
 
